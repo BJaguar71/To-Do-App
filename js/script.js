7e82e83d876e58d6d7abae581490e2d33225fe0a
Maryam Hanifi <87417713+BJaguar71@users.noreply.github.com>
2022-06-13 17:31:05 +0200

    added new item to the list

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 // define a function to add new item 
 function newItem (){
 
+    // add a new item to the list
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+
 }
